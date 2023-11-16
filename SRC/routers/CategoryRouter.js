4370a8909e248949492a7410bbda880d4ee9fb22
Rishi8104 <rishirajshinde1610@gmail.com>
2023-11-16 16:53:32 +0530

    verceldepolye

--- SRC/routers/CategoryRouter.js
+++ SRC/routers/CategoryRouter.js
@@ -1,10 +1,12 @@
 import express from 'express'
-import { FeatchAll, SaveCategory } from '../controller/CategoryCongroller.js';
+import { FeatchAll, FeatchAllActiveCategory, SaveCategory, deleteCategory, fetchDeActiveCategory } from '../controller/CategoryCongroller.js';
 
 const CategoryRouter = express.Router()
 
 CategoryRouter.post('/category', SaveCategory);
 CategoryRouter.get('/category/all', FeatchAll);
-CategoryRouter.post('/category', SaveCategory);
+CategoryRouter.get('/category/active', FeatchAllActiveCategory);
+CategoryRouter.get('/category/Deactive', fetchDeActiveCategory);
+CategoryRouter.delete('/category/:name',deleteCategory);
 
 export default CategoryRouter;
